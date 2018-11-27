6ec48b5195c253040931408b7d106fe1d54b164c
iyinraphael <iyinraphael@gmail.com>
2018-11-26 22:25:40 -0800

    created AudioHelper class and preliminary layout

---
+++ Adaptive Layout/Adaptive Layout/AudioHelper.swift
@@ -0,0 +1,38 @@
+//
+//  AudioHelper.swift
+//  Adaptive Layout
+//
+//  Created by Iyin Raphael on 11/26/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import AVKit
+
+class AudioHelper: NSObject {
+    let player: AVPlayer
+    
+    override init() {
+        // Big Buck Bunny
+        let streamrequest = "https://www.radiantmediaplayer.com/media/bbb-360p.mp4"
+        guard let url = URL(string:streamrequest) else {
+            fatalError("Bad URL configuration")
+        }
+        let asset = AVAsset(url: url)
+        let item = AVPlayerItem(asset: asset)
+        
+        // Create a player member
+        self.player = AVPlayer(playerItem: item)
+        
+        // Init superclass members
+        super.init()
+    }
+    
+    func play() {
+        player.play()
+    }
+    
+    func pause() {
+        player.pause()
+    }
+}
+
